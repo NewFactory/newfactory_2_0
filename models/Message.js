521d7db14b081c825cbf5dd520b793abfd77fab4
NewFactory <10051qazXSW@>
2021-02-10 22:26:57 -0800

    dev

--- models/Message.js
+++ models/Message.js
@@ -3,7 +3,9 @@ const {Schema, model} = require ('mongoose')
 const  schema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
-    text: {type: String, required: true}
+    text: {type: String, required: true},
+    created: {type: Date, default: Date.now, required: true},
+    modified: {type: Date, default: Date.now, required: true}
 })
 
-module.exports = model('Message')
+module.exports = model('Message', schema)
